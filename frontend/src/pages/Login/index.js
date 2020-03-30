4fd437108fc9190ab5be07c5be901d9c46629c56
Felipe <felipe1180@hotmail.com>
2020-03-30 12:09:10 -0300

    added unform for form dealing and yup for form validation

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import Lottie from 'react-lottie';
 import animationData from '../../assets/delivery.json';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 import './styles.css';
 
 export default function Login() {
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Por favor, insira um e-mail válido')
+      .required('O campo e-mail é obrigatório'),
+    pass: Yup.string().min(6).required('O campo senha é obrigatório'),
+  });
+
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -22,14 +35,14 @@ export default function Login() {
         height={450}
       />
       <h1>IFrood</h1>
-      <form>
-        <input type='text' placeholder='E-mail' /> <br />
-        <input type='password' placeholder='Password' /> <br />
+      <Form onSubmit={handleSubmit} schema={schema}>
+        <Input name='email' type='email' placeholder='E-mail' /> <br />
+        <Input name='pass' type='password' placeholder='Password' /> <br />
         <button type='submit'>Sign In</button>
         <span>
-          Não está cadastrado? <a href='teste'>Cadastre-se aqui !</a>
+          Não está cadastrado? <a href='/register'>Cadastre-se aqui !</a>
         </span>
-      </form>
+      </Form>
     </div>
   );
 }
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Lottie from 'react-lottie';
 import animationData from '../../assets/noodles.json';
+import * as Yup from 'yup';
+import { Form, Input } from '@rocketseat/unform';
 import './styles.css';
 
 export default function Register() {
+  const schema = Yup.object().shape({
+    name: Yup.string().required('O campo nome é obrigatório'),
+    email: Yup.string()
+      .email('Preencha um e-mail válido')
+      .required('O campo e-mail é obrigatório'),
+    password: Yup.string().min(6).required('O campo senha é obrigatório'),
+  });
+
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
   const defaultOptions = {
     loop: false,
     autoplay: true,
@@ -16,13 +30,16 @@ export default function Register() {
   return (
     <div className='register-container'>
       <div className='form-side'>
-        <form action=''>
+        <Form onSubmit={handleSubmit} schema={schema}>
           <h1>Cadastro</h1>
-          <input placeholder='Nome' type='text' />
-          <input placeholder='E-mail' type='text' />
-          <input placeholder='Password' type='password' />
+          <Input name='name' placeholder='Nome' type='text' />
+          <Input name='email' placeholder='E-mail' type='text' />
+          <Input name='password' placeholder='Password' type='password' />
           <button type='submit'>Cadastrar!</button>
-        </form>
+          <span>
+            <a href='/'>Já é cadastrado? Clique aqui!</a>
+          </span>
+        </Form>
       </div>
       <div className='lottie-side'>
         <Lottie
