a3e9a5ad297aeafec5b26ac6b320955247661f1b
Felipe <felipe1180@hotmail.com>
2020-03-30 19:56:57 -0300

    added a few loading for ifrood

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Lottie from 'react-lottie';
 import animationData from '../../assets/delivery.json';
+import loading from '../../assets/taco.json';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 import API from '../../services/api';
@@ -15,9 +16,13 @@ export default function Login() {
     password: Yup.string().required('O campo senha é obrigatório'),
   });
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleSubmit = async (values) => {
     try {
+      setIsLoading(true);
       await API.post('http://localhost:3333/login', values);
+      setIsLoading(false);
       history.push('/dashboard');
     } catch (error) {
       history.push('/');
@@ -33,23 +38,44 @@ export default function Login() {
     },
   };
 
+  const loadingOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loading,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
   return (
-    <div className='login-container'>
-      <Lottie
-        options={defaultOptions}
-        isClickToPauseDisabled
-        width={450}
-        height={450}
-      />
-      <h1>IFrood</h1>
-      <Form onSubmit={handleSubmit} schema={schema}>
-        <Input name='email' type='email' placeholder='E-mail' /> <br />
-        <Input name='password' type='password' placeholder='Password' /> <br />
-        <button type='submit'>Sign In</button>
-        <span>
-          Não está cadastrado? <a href='/register'>Cadastre-se aqui !</a>
-        </span>
-      </Form>
-    </div>
+    <>
+      {isLoading ? (
+        <Lottie
+          options={loadingOptions}
+          width={400}
+          height={400}
+          style={{ marginTop: '50px' }}
+        />
+      ) : (
+        <div className='login-container'>
+          <Lottie
+            options={defaultOptions}
+            isClickToPauseDisabled
+            width={450}
+            height={450}
+          />
+          <h1>IFrood</h1>
+          <Form onSubmit={handleSubmit} schema={schema}>
+            <Input name='email' type='email' placeholder='E-mail' /> <br />
+            <Input name='password' type='password' placeholder='Password' />
+            <br />
+            <button type='submit'>Sign In</button>
+            <span>
+              Não está cadastrado? <a href='/register'>Cadastre-se aqui !</a>
+            </span>
+          </Form>
+        </div>
+      )}
+    </>
   );
 }
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Lottie from 'react-lottie';
 import animationData from '../../assets/noodles.json';
+import loading from '../../assets/taco.json';
 import * as Yup from 'yup';
 import { Form, Input } from '@rocketseat/unform';
 import './styles.css';
@@ -16,9 +17,13 @@ export default function Register() {
     password: Yup.string().min(6).required('O campo senha é obrigatório'),
   });
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleSubmit = async (values) => {
     try {
+      setIsLoading(true);
       await API.post('http://localhost:3333/users', values);
+      setIsLoading(false);
     } catch (error) {
       console.log('deu erro');
     }
@@ -33,29 +38,51 @@ export default function Register() {
     },
   };
 
+  const loadingOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loading,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
   return (
-    <div className='register-container'>
-      <div className='form-side'>
-        <Form onSubmit={handleSubmit} schema={schema}>
-          <h1>Cadastro</h1>
-          <Input name='name' placeholder='Nome' type='text' />
-          <Input name='email' placeholder='E-mail' type='text' />
-          <Input name='password' placeholder='Password' type='password' />
-          <button type='submit'>Cadastrar!</button>
-          <span>
-            Já é cadastrado? <a href='/'>Clique aqui!</a>
-          </span>
-        </Form>
-      </div>
-      <div className='lottie-side'>
+    <>
+      {isLoading ? (
         <Lottie
-          options={defaultOptions}
-          width={500}
-          height={500}
-          isClickToPauseDisabled
-          speed={1}
+          options={loadingOptions}
+          width={400}
+          height={400}
+          style={{ marginTop: '50px' }}
         />
-      </div>
-    </div>
+      ) : (
+        <>
+          <div className='register-container'>
+            <div className='form-side'>
+              <Form onSubmit={handleSubmit} schema={schema}>
+                <h1>Cadastro</h1>
+                <Input name='name' placeholder='Nome' type='text' />
+                <Input name='email' placeholder='E-mail' type='text' />
+                <Input name='password' placeholder='Password' type='password' />
+                <button type='submit'>Cadastrar!</button>
+                <span>
+                  Já é cadastrado? <a href='/'>Clique aqui!</a>
+                </span>
+              </Form>
+            </div>
+            <div className='lottie-side'>
+              <Lottie
+                options={defaultOptions}
+                width={500}
+                height={500}
+                isClickToPauseDisabled
+                speed={1}
+              />
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 }
